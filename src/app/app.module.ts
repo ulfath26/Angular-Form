219f08d203c69a8c434f9e45027c5a5131bd558b
ulfath26 <67775953+ulfath26@users.noreply.github.com>
2021-02-26 21:30:25 +0530

    Responsive form in Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { CourseListComponent } from "./course-list/course-list.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SignUpComponent,
+    CourseListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
